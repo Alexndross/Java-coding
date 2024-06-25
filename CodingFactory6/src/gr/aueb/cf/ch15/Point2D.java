5a4120bdbbac2189ad3cea73b4926d142eff1317
Alex Stavropoulos <arthex4@gmail.com>
2024-06-26 01:31:28 +0300

    Changes

--- CodingFactory6/src/gr/aueb/cf/ch15/Point2D.java
+++ CodingFactory6/src/gr/aueb/cf/ch15/Point2D.java
@@ -13,10 +13,34 @@ public class Point2D extends Point {
         this.y = y;
     }
 
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
     @Override
     public String convertToString() {
         return "(" + getX() +  "," + y + ")";
     }
 
+    @Override
+    public void movePlus10() {
+        super.movePlus10();
+        y += 10;
+    }
 
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        y +=1;
+    }
+
+    @Override
+    protected void reset() {
+        super.reset();
+        y = 0;
+    }
 }
